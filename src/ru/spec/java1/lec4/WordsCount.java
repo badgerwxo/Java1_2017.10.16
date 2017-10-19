fd9b5288762c3ef9600ad782025d6fa1b49670a8
student <student@tg6-01>
2017-10-19 20:01:40 +0300

    init

---
+++ src/ru/spec/java1/lec4/WordsCount.java
@@ -0,0 +1,42 @@
+package ru.spec.java1.lec4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class WordsCount {
+	public static void main(String[] args) {
+		String s1 = "	бла бла бла  	";
+		String s2 = "	бла бла бла  drtb ervt	aiuwyet oytwb iu8yt 222 333 4444";
+		s1 = s1.trim().replaceAll("\\s+", " ");
+		// first, trim from both sides; second, 1 and more spaces replace with 1 space
+
+		ArrayList<String> strings = new ArrayList<>();
+		TreeSet<String> set = new TreeSet<>();
+		
+		strings.size();
+		
+		addWordsToArray(s1, strings);
+		addWordsToArray(s2, strings);
+		
+		set.addAll(strings);
+
+//		for (String word : strings) { // for each cycle
+//			System.out.println(word);
+//		}
+		
+		for (String word : set) { // вывод уникальных значений
+			System.out.println(word);
+		}
+
+		//System.out.println(split.length);
+		System.out.println(set.size());//количество уникальных значений
+	}
+
+	private static String[] addWordsToArray(String s1, ArrayList<String> strings) {
+		String[] split = s1.trim().split("\\s+");
+		strings.addAll(Arrays.asList(split));
+		return split;
+	}
+
+}
