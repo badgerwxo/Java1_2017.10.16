957ccac6767f10d40f49a05f73ecb059360ad722
student <student@tg6-01>
2017-10-23 21:22:35 +0300

    System in

--- MyFirstProject/src/ru/spec/java1/lec6/FilesTest.java
+++ MyFirstProject/src/ru/spec/java1/lec6/FilesTest.java
@@ -1,14 +1,22 @@
 package ru.spec.java1.lec6;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Scanner;
 
 
 public class FilesTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println("Enter any string");
 		InputStream in = 		System.in;
+		
+		File file = new File("C:/java/test.txt");
+		in = new FileInputStream(file);
+		
+		
 		Scanner scanner = new Scanner(in);
 		
 		for (;scanner.hasNextLine();) {
