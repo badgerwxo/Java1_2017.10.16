12ae82ca413c138dcc87529e474267cd51383397
student <student@tg6-01>
2017-10-23 19:21:23 +0300

    Palindrom

--- MyFirstProject/src/ru/spec/java1/lec6/Palindrom.java
+++ MyFirstProject/src/ru/spec/java1/lec6/Palindrom.java
@@ -3,10 +3,47 @@ package ru.spec.java1.lec6;
 public class Palindrom {
 
 	public static void main(String[] args) {
-		System.out.println(Long.toString(5, 10));//10ричная система
-		System.out.println();
-		
 
+		for (int i=0;i<100000;i++) {
+			if(isPalindrom(i, 7)&& isPalindrom(i, 17)) {
+				System.out.println("("
+						+i
+						+")"
+						+Long.toString(i,7)
+						+" "
+						+Long.toString(i,17));
+			}
+			
+		}
+			
+		}
+		
+		
+	private static boolean isPalindrom(long m, int r) {	
+		String string;
+		string = Long.toString(m,r);
+		return isPalindrom (string);
+		}
+	
+	
+	
+	
+	
+	
+	
+	private static boolean isPalindrom(String string) {
+		boolean isPal;
+		
+		int lengh = string.length() / 2;
+		for (int i = 0; i < lengh; i++) {
+
+			if (string.charAt(i) != string.charAt(string.length() - 1 - i)) {
+				return false;
+
+			}
+
+		}
+		return true;
 	}
 
 }
