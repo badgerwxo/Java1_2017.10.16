e2bf893524f094cc2569509de886469a1ee1b641
Lyubov Piyavkina <Lyubov Piyavkina@192.168.0.11>
2017-10-24 01:23:51 +0300

    System in

--- MyFirstProject/src/ru/spec/java1/lec6/FileHW.java
+++ MyFirstProject/src/ru/spec/java1/lec6/FileHW.java
@@ -8,15 +8,16 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeSet;
 
 
-public class FilesTest {
+public class FileHW {
 
 	public static void main(String[] args) throws FileNotFoundException   {
-		System.out.println("Enter any string");
+		//System.out.println("Enter any string");
 		InputStream in = 		System.in;
 		
 		File file = new File("C:/java/java1.txt");
@@ -30,7 +31,8 @@ public class FilesTest {
 		
 		ArrayList<String> strings = new ArrayList<>();
 		strings.listIterator().previousIndex();
-		TreeSet<String> set = new TreeSet<>();
+		//TreeSet<String> set = new TreeSet<>();
+		HashSet<String> set = new HashSet<>();
 		
 		
 		for (;scanner.hasNextLine();) {
@@ -38,19 +40,28 @@ public class FilesTest {
 		String [] nextLine = scanner.nextLine().toLowerCase().split("[^а-яё]+");
 			strings.addAll(Arrays.asList(nextLine));
 			
-			
-			
-			
 		//System.out.println(nextLine);
 		}
-		set.addAll(strings);
+//		System.out.println(strings.size());
+//		for (int i=0;i<=strings.size();i++) {
+			set.addAll(strings);
+//			int wordsCount=1;
+//			for (String word:set) {
+//				if (strings.equals(word)){
+//					wordsCount++;
+//					System.out.println(strings + " " + wordsCount);
+//				}
+//			}
+//		}
+		
+		
 		for (String word : set) { // добавляет уникальные значения
 			
-			System.out.print (word + "(" +" "+ ")");
+			System.out.println (word + "(" +word.length()+ ")");
 		}
 ///
 		//System.out.println(split.length);
-		System.out.println(set.size());
+		System.out.println(set.size()+ "ээ");
 	}
 
 }
