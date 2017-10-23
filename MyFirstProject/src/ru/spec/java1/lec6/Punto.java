072ef5b865a0720ddb8a135e63759b77297a6fba
student <student@tg6-01>
2017-10-23 20:43:34 +0300

    Punto

--- MyFirstProject/src/ru/spec/java1/lec6/Punto.java
+++ MyFirstProject/src/ru/spec/java1/lec6/Punto.java
@@ -4,14 +4,13 @@ public class Punto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(ruToEn("Ынынуь щге"));
 	}
-	
-	
+
 	static public String ruToEn(String ru) {
-		return ru;//FIXME not impl
+		return ru;// FIXME not impl
 		
-		
+
 	}
 
 }
