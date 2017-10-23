a93e90b2c82ee4e4383422706f8c1c7f254ebb78
student <student@tg6-01>
2017-10-23 20:18:21 +0300

    MapTest

---
+++ MyFirstProject/src/ru/spec/java1/lec6/MapTest.java
@@ -0,0 +1,40 @@
+package ru.spec.java1.lec6;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.function.BiConsumer;
+import java.util.Set;
+
+
+public class MapTest {// интерфейс,
+	// map относится к коллекциям, но не является коллекцией
+
+	public static void main(String[] args) {
+		HashMap<String, String> map = new HashMap<>();
+		map.put("Hello", "world");
+		map.put("Bye", "123");
+		map.put("hello", "WORLD");
+		System.out.println(map.put("hello", "WORLD!"));// озвращает предудущее значение
+
+		map.put("123", "1");
+		map.put("234", "2");
+		map.put("345", "3");
+
+		System.out.println(map);
+		Set <Entry<String, String>> entrySet = map.entrySet();
+		for (Entry<String,String> entry:entrySet) {
+			System.out.println(entry.getKey());
+			System.out.println(entry.getValue());
+			System.out.println();
+			
+		}
+		
+//		Set<String> keySet=map.keySet();
+//		Collection<String> values = map.values();
+		
+
+		map.forEach((key,val)-> System.out.println(key+" =>" +val));
+	}
+
+}
