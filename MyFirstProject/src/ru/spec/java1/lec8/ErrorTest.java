5cf0b9ec57eff58cde39c6577a55dba443313079
student <student@tg6-01>
2017-10-25 20:01:38 +0300

    Errors

---
+++ MyFirstProject/src/ru/spec/java1/lec8/ErrorTest.java
@@ -0,0 +1,13 @@
+package ru.spec.java1.lec8;
+
+public class ErrorTest {
+
+	public static void main(String[] args) {
+		System.out.println("Start");
+		int i = 5/0;
+		System.out.println("end");
+		// TODO Auto-generated method stub
+
+	}
+
+}
